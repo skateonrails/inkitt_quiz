3bfaa2d3b28e2ef9363a6a72901b311cc4174065
Samuel Prado <darkms@gmail.com>
2017-10-02 23:08:47 -0300

    Add helper methods for Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,12 @@ class Question < ApplicationRecord
   has_many :alternatives, -> { order(position: :asc) }
 
   validates :title, presence: true, uniqueness: true
+
+  def next_question
+    children.first
+  end
+
+  def previous_question
+    parent
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -17,4 +17,35 @@ RSpec.describe Question, type: :model do
       expect(subject.children.include?(new_question)).to be true
     end
   end
+
+  describe '#next_question' do
+    context 'without child question' do
+      it { expect(subject.next_question).to be_nil }
+    end
+
+    context 'with child' do
+      it 'should return first child' do
+        new_question = create(:question)
+
+        subject.add_child new_question
+
+        expect(subject.next_question).to eq(new_question)
+      end
+    end
+  end
+
+  describe '#previous_question' do
+    context 'without parent question' do
+      it { expect(subject.previous_question).to be_nil }
+    end
+
+    context 'with parent' do
+      it 'should return first child' do
+        new_question = create(:question)
+        subject.parent = new_question
+
+        expect(subject.previous_question).to eq(new_question)
+      end
+    end
+  end
 end
